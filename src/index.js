0b6b1c9ef398b33ef0b27a76e66f5b25ca45195c
NastyaKvachenok <nastya.kvachenok@gmail.com>
2020-10-21 22:09:55 +0300

    feat: imlement towel-sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let sortedArray = [];
+  if (Array.isArray(matrix)) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 != 0 ) {
+        matrix[i] = matrix[i].reverse();
+      }
+      for (let j = 0; j < matrix[i].length; j++) {
+        sortedArray.push(matrix[i][j]);
+      }
+    }
+  } 
+  return sortedArray;
 }
